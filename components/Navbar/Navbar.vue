f1f395cc2d85bc92dff9fbb85b0280c033a4547e
Sofia Costa <sofiacosta2908@gmail.com>
2021-08-09 12:12:46 +0200

    Install tailwind, started navbar and firebase config

---
+++ components/Navbar/Navbar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="flex justify-around w-full h-20 items-center">
+    <nuxt-link
+      v-for="element in navbarElements"
+      :key="element.id"
+      :to="element.link"
+      class="uppercase font-thin hover:text-gray-500"
+      >{{ element.name }}</nuxt-link
+    >
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+interface NavbarElement {
+  link: string;
+  name: string;
+  id: number;
+}
+
+export default Vue.extend({
+  data() {
+    const navbarElements: NavbarElement[] = [
+      {
+        link: "/about",
+        name: "About Me",
+        id: 0,
+      },
+      {
+        link: "/fashion",
+        name: "Fashion",
+        id: 0,
+      },
+      {
+        link: "/events",
+        name: "Events",
+        id: 0,
+      },
+      {
+        link: "/personal",
+        name: "Personal",
+        id: 0,
+      },
+      {
+        link: "/product",
+        name: "Product",
+        id: 0,
+      },
+      {
+        link: "/video",
+        name: "Video",
+        id: 0,
+      },
+      // Not sure yet
+      {
+        link: "/reviews",
+        name: "Reviews",
+        id: 0,
+      },
+    ];
+    return {
+      navbarElements,
+    };
+  },
+});
+</script>
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -5,13 +5,12 @@ import 'firebase/database'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: "AIzaSyDpPTCfFjyV9QKGArHpq67iS6NO-uYbQbM",
+  authDomain: "ffportfolio-99d42.firebaseapp.com",
+  projectId: "ffportfolio-99d42",
+  storageBucket: "ffportfolio-99d42.appspot.com",
+  messagingSenderId: "278623507510",
+  appId: "1:278623507510:web:bc841fc31a3631bede7497"
 }
 // Initialize Firebase
 
