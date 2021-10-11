4c4ad25387c5bd51133dd902e7680f9bf739123f
Sofia Costa <>
2021-10-11 14:38:31 +0200

    Project item created

---
+++ components/Project/ProjectItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      {{ title }}
+    </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: { 
+    project: {
+      required: true,
+      default: {
+        id: 0,
+        date: '',
+        description: '',
+        images: [],
+        images_preview: [],
+        slug: '',
+        subtitle: '',
+        title: '',
+        tags: [],
+      }
+    } 
+  },
+  data() { return { ...this.project }}
+});
+</script>
