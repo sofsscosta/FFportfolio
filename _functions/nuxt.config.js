d9dc6e6adec0a6d923586f3e1a7c3feeffa5b072
Sofia Costa <>
2021-10-15 19:05:31 +0200

    Starting hosting and deploying, need access to cloud functions to proceed

---
+++ _functions/nuxt.config.js
@@ -0,0 +1,129 @@
+require('dotenv').config()
+
+export default {
+  dir: {
+    store: 'store'
+  },
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'ffportfolio',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['~/assets/main.css', '~/assets/formulate.css'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    './plugins/firebase.js',
+    '~/plugins/vue-formulate',
+    '~/plugins/vue-zoomer',
+    { src: '~/plugins/masonry', mode: 'client' },
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/dotenv',
+    // '@braid/vue-formulate/nuxt'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/robots',
+    '@nuxtjs/dotenv',
+    '@nuxtjs/toast',
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: process.env.API_KEY,
+          authDomain: process.env.AUTH_DOMAIN,
+          projectId: process.env.PROJECT_ID, 
+          storageBucket: process.env.STORAGE_BUCKET, 
+          messagingSenderId: process.env.MESSAGING_SENDER_ID,
+          appId: process.env.APP_ID
+        },
+        services: {
+          auth: {
+            persistence: 'local', // default
+            initialize: {
+              onAuthStateChangedAction: 'onAuthStateChanged',
+              subscribeManually: false
+            },
+            ssr: false,
+          },
+          firestore: true,
+          storage: true,
+      }
+      }
+    ]
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    babel:{
+      plugins: [
+        ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]      
+      ]
+    },
+    extend(config, {}) {
+      config.node = {
+          fs: 'empty'
+      }
+    }
+  },
+  storage: true,
+  pageTransition: {
+    name: 'page',
+    mode: 'out-in',
+  },
+  formulate: {
+    configPath: '~/formulate.config.js'
+  },
+  robots: {
+    UserAgent: '*',
+    Disallow: `/${process.env.ADMIN_PATH}`,
+  },
+  // router: {
+  //   middleware: ['error'],
+  // },
+  hooks: {
+    render: {
+      errorMiddleware(app) {
+        app.use((error, _req, _res, next) => {
+          console.log(error)
+          if (error) {
+            console.log('app in error',app)
+          }
+          next(error);
+        });
+      },
+    },
+  },
+  toast: {
+    position: 'top-center'
+}
+  // TO SEE ON MOBILE LIVE
+  // server: {     
+  //   port: 8000, // default: 3000     
+  //   host: '0.0.0.0', // default: localhost   
+  // }, 
+}
