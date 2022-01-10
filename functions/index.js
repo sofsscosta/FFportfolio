70cb5fefffa452ffdda92d94b7c46ea7debc1385
Sofia Costa <>
2022-01-10 17:08:04 +0100

    delete comment

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,3 @@
-// const functions = require('firebase-functions')
-
-// // // Create and Deploy Your First Cloud Functions
-// // // https://firebase.google.com/docs/functions/write-firebase-functions
-// //
-// // exports.helloWorld = functions.https.onRequest((request, response) => {
-// //  response.send("Hello from Firebase!");
-// // });
-
-// const { Nuxt } = require('nuxt-start')
-
-// const nuxtConfig = require('./nuxt.config.js')
-
-// const config = {
-//   ...nuxtConfig,
-//   dev: false,
-//   debug: false,
-//   buildDir: 'nuxt',
-// }
-// const nuxt = new Nuxt(config)
-
-// exports.ssrapp = functions.https.onRequest(async (req, res) => {
-//   await nuxt.ready()
-//   nuxt.render(req, res)
-// })
-
-
 const functions = require('firebase-functions')
 const { Nuxt } = require('nuxt')
 const express = require('express')
